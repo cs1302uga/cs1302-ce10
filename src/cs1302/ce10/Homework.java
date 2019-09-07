2a6b7b5e2dbc7560e11316eeae14155708dd8c73
bjb211 <bjb211@uga.edu>
2019-09-07 12:44:50 -0400

    Fixing style issues

--- src/cs1302/ce10/Homework.java
+++ src/cs1302/ce10/Homework.java
@@ -10,13 +10,14 @@ import java.util.Date;
  * @version 1.0; Feb 9 2019
  */
 public class Homework {
-    /** Total point value for this homework */
+
+    /** Total point value for this homework. */
     private int totalPoints;
-    /** Indicates if this is a bonus assignment */
+    /** Indicates if this is a bonus assignment. */
     private boolean isBonus;
-    /** Course where this homework is assigned */
+    /** Course where this homework is assigned. */
     private Course course;
-    /** Due date of this homework assignment */
+    /** Due date of this homework assignment. */
     private Date dueDate;
 
     /**
@@ -27,37 +28,37 @@ public class Homework {
      * @param bonus indicates if this is a bonus homework.
      * @param course the course where this homework is assigned.
      * @param dueDate the {@code java.util.Date} this homework is due.
-     * 
+     *
      * @throws IllegalArgumentException if {@code points} is negative
      * or if {@code dueDate} or {@code course} is {@code null}.
      */
     public Homework(int points, boolean bonus, Course course,
-		    Date dueDate) {
-	nullCheck("Homework", course, dueDate);
+                    Date dueDate) {
+        nullCheck("Homework", course, dueDate);
 
-	if(points < 0) {
-	    throw new IllegalArgumentException("Homework Constructor: " +
-				      "Invalid homework point total");
-	} // if
-	this.isBonus = bonus;
-	this.totalPoints = points;
-	this.course = course;
-	this.dueDate = dueDate;
+        if (points < 0) {
+            throw new IllegalArgumentException("Homework Constructor: " +
+                                               "Invalid homework point total");
+        } // if
+        this.isBonus = bonus;
+        this.totalPoints = points;
+        this.course = course;
+        this.dueDate = dueDate;
     } // Homework
 
     /**
      * Updates the point total associated with this homework.
      *
      * @param points the new point total.
-     * @throws IllegalArgumentException if {@code points} 
+     * @throws IllegalArgumentException if {@code points}
      * is negative.
      */
     public void setPoints(int points) {
-	if(points < 0) {
-	    throw new IllegalArgumentException("Homework Constructor: " +
-				      "Invalid homework point total");
-	} // if
-	this.totalPoints = points;
+        if (points < 0) {
+            throw new IllegalArgumentException("Homework Constructor: " +
+                                               "Invalid homework point total");
+        } // if
+        this.totalPoints = points;
     } // setPoints
 
     /**
@@ -66,18 +67,18 @@ public class Homework {
      * @param bonus the bonus indicator.
      */
     public void setBonus(boolean bonus) {
-	this.isBonus = bonus;
+        this.isBonus = bonus;
     } // setBonus
 
     /**
      * Updates the due date of this homework assignment.
-     * 
+     *
      * @param due the new due {@code java.util.Date}.
      * @throws NullPointerException if {@code due} is {@code null}.
      */
     public void setDueDate(Date due) {
-	nullCheck("setDueDate", due);
-	this.dueDate = due;
+        nullCheck("setDueDate", due);
+        this.dueDate = due;
     } // setDueDate
 
     /**
@@ -86,18 +87,18 @@ public class Homework {
      * @return the {@code java.util.Date} this homework is due.
      */
     public Date getDueDate() {
-	return this.dueDate;
+        return this.dueDate;
     } // getDueDate
-    
+
     /**
      * Returns whether or not this homework is bonus.
      *
      * @return if this homework is a bonus assignment.
      */
     public boolean isBonus() {
-	return isBonus;
+        return isBonus;
     } // isBonus
-    
+
     /**
      * Returns the number of points associated with this homework
      * assignment.
@@ -105,7 +106,7 @@ public class Homework {
      * @return the points associated with this {@code Homework} object.
      */
     public int getPoints() {
-	return totalPoints;
+        return totalPoints;
     } // getPoints
 
     /**
@@ -115,28 +116,27 @@ public class Homework {
      * homework.
      */
     public Course getCourse() {
-	return course;
+        return course;
     } // getCourse
-    
+
     /**
      * Throws a NullPointerException if any values in the
-     * varargs parameter, {@code o} are {@code null}. The 
-     * method performs no actions if all values in {@code o} 
+     * varargs parameter, {@code o} are {@code null}. The
+     * method performs no actions if all values in {@code o}
      * are non-null.
      *
      * @param method the name of the calling method.
      * @param o varargs parameter containing all objects to verify
-     * @throws NullPointerException if any element of parameter 
+     * @throws NullPointerException if any element of parameter
      * {@code o} is null.
      */
     private void nullCheck(String method, Object ... o) {
-	for(Object obj: o) {
-	    if(o == null) {
-		throw new NullPointerException(method +
-				       ": Null Argument Provided");
-	    } // if
-	} // for
+        for (Object obj: o) {
+            if (o == null) {
+                throw new NullPointerException(method +
+                                               ": Null Argument Provided");
+            } // if
+        } // for
     } // checkNull
 
 } // Homework
-
--- src/cs1302/ce10/Quiz.java
+++ src/cs1302/ce10/Quiz.java
@@ -1,14 +1,14 @@
 package cs1302.ce10;
 
 /**
- * Represents a quiz in the cs1302 mock eLC
- * application.
+ * Represents a quiz in the cs1302 mock eLC application.
  *
  * @author Brad Barnes
  * @version 1.0; Feb 9 2019
  */
 public class Quiz {
-    /** Time (in minutes) students have to complete the quiz */
+
+    /** Time (in minutes) students have to complete the quiz. */
     private int timeLimit;
     /** Total point value for this quiz. */
     private int points;
@@ -34,11 +34,11 @@ public class Quiz {
                 int limit) {
         nullCheck("Quiz Constructor", course);
 
-        if(points < 0) {
+        if (points < 0) {
             throw new IllegalArgumentException("Quiz Constructor: " +
                                                "Invalid quiz point total");
         } // if
-        if(limit < 0) {
+        if (limit < 0) {
             throw new IllegalArgumentException("Quiz Constructor: " +
                                                "Invalid time limit");
         } // if
@@ -56,7 +56,7 @@ public class Quiz {
      * is negative.
      */
     public void setPoints(int pointValue) {
-        if(pointValue < 0) {
+        if (pointValue < 0) {
             throw new IllegalArgumentException("Quiz Constructor: " +
                                                "Invalid quiz point total");
         } // if
@@ -79,7 +79,7 @@ public class Quiz {
      * @throws IllegalArgumentException if {@code limit} is negative.
      */
     public void setTimeLimit(int limit) {
-        if(limit < 0) {
+        if (limit < 0) {
             throw new IllegalArgumentException("setTimeLimit: " +
                                                "Invalid time limit");
         } // if
@@ -136,8 +136,8 @@ public class Quiz {
      * {@code o} is null.
      */
     private void nullCheck(String method, Object ... o) {
-        for(Object obj: o) {
-            if(o == null) {
+        for (Object obj: o) {
+            if (o == null) {
                 throw new NullPointerException(method +
                                                ": Null Argument Provided");
             } // if
